3729748e3b5d79f9b31ceff7fb0f6d01c12bfecd
Shubham Nikam <nikamshubham224@gmail.com>
2024-04-17 13:46:58 +0530

    Setting up Multer storage path

--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,30 @@
 import  {Router} from "express";
 const router = Router();
+import multer from "multer";
+import path from "path"
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve('./public/upload'))
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+
 
 
 router.get('/add-new', (req,res)=>{
     return  res.render('addBlog', {
         user: req.user,
     });
+})
+router.post("/",(req,res)=>{
+    console.log(req.body);
 })
 
 export default router;
